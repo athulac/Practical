b81eb0de73164326ef430cb66eaadee0790bcb7a
athulac <caathu@gmail.com>
2023-07-14 04:42:50 +0530

    practical

---
+++ Chinook/ViewModels/UserPlaylistViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Chinook.ViewModels
+{
+    public class UserPlaylistViewModel
+    {
+        public List<string> Names { get; set; } = new List<string> { };
+
+    }
+}
---
+++ Chinook/wwwroot/script/site.js
@@ -0,0 +1,23 @@
+﻿
+
+function showAlert(message) {
+    alert(message);
+}
+
+
+
+function getComponentPartial() {
+    try {
+        $.get({
+            url: '../Shared/Components/Component',
+            dataType: 'HTML',
+            success: function (data) {
+                $('#componentPlaceholder').html(data);
+            }
+        });
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+}
+
